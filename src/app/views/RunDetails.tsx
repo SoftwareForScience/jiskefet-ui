cfb1ea05ee662a7b606d7c4559804d7421a1bb93
Bastiaan Reinalda <bastiaanreinalda@Bastiaans-MBP.fritz.box>
2018-10-02 18:29:41 +0200

    Made cardbody as a component

--- src/app/views/RunDetails.tsx
+++ src/app/views/RunDetails.tsx
@@ -2,13 +2,13 @@
  * TODO:
  * - Fix timestamps from Run
  * - Discuss visualisations
- * - Use no mocup date 
- * - Make components of bordered columns
+ * - Use no mocup date
  */
 
 import * as m from 'mithril';
 import RunModel from '../models/Run';
 import Spinner from '../components/Spinner';
+import CardBody from '../components/CardBody';
 
 export class RunDetails implements m.Component {
     private isLoading: boolean;
@@ -31,42 +31,40 @@ export class RunDetails implements m.Component {
                     {RunModel.current &&
                         <div className="col-md-10 mx-auto">
                             <div className="row">
-                                <div className="col-md-4 border rounded border-dark">
-                                    <h4>Run</h4>
-                                    {Object.keys(RunModel.current).map((key) =>
-                                        (<p key={RunModel.current[key]}>
-                                            {key}: {RunModel.current[key]}
-                                        </p>)
-                                    )}
+                                <div className="col-md-4">
+                                    <div className="card">
+                                        <h4 className="card-header">Run</h4>
+                                        <CardBody run={RunModel.current} />
+                                    </div>
                                 </div>
                                 <div className="col-md-7 offset-md-1">
                                     <div className="row">
-                                        <div className="col-md-5 border rounded border-dark">
-                                            <h4>Detectors</h4>
-                                            {
-                                                //Fetch detectors from runs
-                                            }
+                                        <div className="col-md-5">
+                                            <div className="card">
+                                                <h4 className="card-header">Detectors</h4>
+                                                <CardBody run={"test"} />
+                                            </div>
                                         </div>
-                                        <div className="col-md-5 offset-md-2 border rounded border-dark">
-                                            <h4>EPN Role Sessions</h4>
-                                            {
-                                                //Fetch epn role from runs
-                                            }
+                                        <div className="col-md-5 offset-md-2">
+                                            <div className="card">
+                                                <h4 className="card-header">EPN Role Sessions</h4>
+                                                <CardBody run={"test"} />
+                                            </div>
                                         </div>
                                     </div>
                                     <br />
                                     <div className="row">
-                                        <div className="col-md-5 border rounded border-dark">
-                                            <h4>FLP Roles</h4>
-                                            {
-                                                //Fetch flp roles from runs
-                                            }
+                                        <div className="col-md-5">
+                                            <div className="card">
+                                                <h4 className="card-header">EPN Role Sessions</h4>
+                                                <CardBody run={"test"} />
+                                            </div>
                                         </div>
-                                        <div className="col-md-5 offset-md-2 border rounded border-dark">
-                                            <h4>Run Quality history</h4>
-                                            {
-                                                //Fetch run quality history from runs
-                                            }
+                                        <div className="col-md-5 offset-md-2">
+                                            <div className="card">
+                                                <h4 className="card-header">Run Quality history</h4>
+                                                <CardBody run={"test"} />
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
