881f5ac5d53ae84c3fef77da4be2047cdd413425
misharigot <misharigot@gmail.com>
2018-10-03 03:10:53 +0200

    Revamped topbar

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -4,13 +4,21 @@ import 'bootstrap';
 import { Layout } from './Layout';
 import { Runs } from './views/Runs';
 import { Logs } from './views/Logs';
+import { Log } from './views/Log';
 import { Create } from './views/Create';
 
-m.route(document.body, '/', {
-    '/': {
+m.route(document.body, '/logs', {
+    '/logs': {
         view: () => (
             <Layout>
                 <Logs />
+            </Layout>
+        ),
+    },
+    '/logs/:id': {
+        view: (vnode: any) => (
+            <Layout>
+                <Log id={vnode.attrs.id} />
             </Layout>
         ),
     },
