b277698331d43985d658328fe6394855ef87e9d4
Calvin <huynhck001@gmail.com>
2018-10-09 16:53:51 +0200

    nav items can now take an optional icon parameter, items can be added to the right side of the navbar

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -16,18 +16,18 @@ export class NavBar implements m.Component {
             <nav class="navbar navbar-expand-sm navbar-dark jiskefet-navbar" >
                 <div class="container-fluid">
                     <div class="navbar-header w-100 d-flex">
-                        <a href="/" class="navbar-brand" oncreate={m.route.link}>
+                        <a href="/" class="navbar-brand jiskefet-navbar-text" oncreate={m.route.link}>
                             <img src="../../assets/cern_logo.png" width="30" height="30" class="d-inline-block align-top logo" alt="" />
                             Jiskefet
                         </a>
-                        <button type="button" class="unstyled-button" id="menu-toggle" onclick={toggle}><span class="navbar-toggler-icon" /></button>
-                        {/* <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon" />
-                        </button> */}
+                        <button type="button" class="unstyled-button" id="menu-toggle" onclick={toggle}><span class="fas fa-bars" /></button>
+                        <button class="navbar-toggler navbar-nav ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="fas fa-user-cog" />
+                        </button>
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul class="navbar-nav ml-auto">
-                                <NavItem href="/#" name="Login" />
                                 <NavItem href="/#" name="Register" />
+                                <NavItem href="/#" name="Login" icon="fa-user" />
                             </ul>
                         </div>
                     </div>
--- src/app/components/NavItem.tsx
+++ src/app/components/NavItem.tsx
@@ -3,17 +3,30 @@ import * as m from 'mithril';
 export class NavItem implements m.Component {
     private href: string;
     private title: string;
+    private icon: string;
 
     constructor(vnode: any) {
         this.href = vnode.attrs.href;
         this.title = vnode.attrs.name;
+        this.icon = vnode.attrs.icon;
     }
 
     view() {
+        let text;
+        if (this.icon === undefined) {
+            text = <a href={this.href} className="nav-link" oncreate={m.route.link}>{this.title}</a>;
+        } else {
+            text = [(
+                <a href={this.href} className="nav-link" oncreate={m.route.link}>
+                    <span class={`fas ${this.icon}`} />
+                    &nbsp;{this.title}</a>
+            )];
+        }
+
         return (
             <li class={`nav-item ${this.href === m.route.get() && 'active'}`}>
-                <a href={this.href} className="nav-link" oncreate={m.route.link}>{this.title}</a>
-            </li>
+                {text}
+            </li >
         );
     }
 }
