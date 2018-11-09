bb22f153d49828042450e12d1629461f00f49ad8
Misha Rigot <misharigot@gmail.com>
2018-11-09 12:23:23 +0100

    Removed console.log.

--- src/app/components/Table.tsx
+++ src/app/components/Table.tsx
@@ -21,8 +21,6 @@ type Vnode = m.Vnode<Attrs, Table>;
 export default class Table extends MithrilTsxComponent<Attrs> {
     view(vnode: Vnode) {
         const { columns, className, data } = vnode.attrs;
-        console.log(data);
-
         return (
             <div class="table-responsive">
                 <table class={`table table-sm table-bordered table-hover shadow-sm ${className || ''}`}>
