aa80a72d6e384dbf3515ac348474b78b2511c17d
Bastiaan Reinalda <bastiaanreinalda@Bastiaans-MBP.fritz.box>
2019-01-18 18:29:43 +0100

    Used card component at Log.tsx

--- src/app/pages/Log.tsx
+++ src/app/pages/Log.tsx
@@ -17,6 +17,7 @@ import Tabs from '../molecules/Tab';
 import Modal from '../atoms/Modal';
 import LinkRunToLog from '../atoms/LinkRunToLog';
 import SuccessMessage from '../atoms/SuccessMessage';
+import Card from '../atoms/Card';
 
 interface Attrs {
     logId: number;
@@ -42,79 +43,77 @@ export default class Log extends MithrilTsxComponent<Attrs> {
                         <SuccessMessage />
                         <div class="row">
                             <div class="col-md-12 mx-auto">
-                                <div class="card shadow-sm bg-light">
-                                    <div class="card-header">
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <h3>Log</h3>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <div class="row justify-content-end">
-                                                    <button
-                                                        type="button"
-                                                        class="btn btn-primary btn-sm mr-1"
-                                                        data-toggle="modal"
-                                                        data-target={`#${addExistingRunId}`}
-                                                    >
-                                                        Link existing run
-                                                    </button>
+                                <Card
+                                    className={['shadow-sm', 'bg-light']}
+                                    headerTitle={'Log'}
+                                    headerContent={(
+                                        <div class="row justify-content-end">
+                                            <button
+                                                type="button"
+                                                class="btn btn-primary btn-sm mr-1"
+                                                data-toggle="modal"
+                                                data-target={`#${addExistingRunId}`}
+                                            >
+                                                Link existing run
+                                            </button>
+                                        </div>
+                                    )}
+                                >
+                                    <div>
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <h5 class="card-title">{State.LogModel.current.title}</h5>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <dl class="row">
+                                                        <dt class="col-sm-6">Log id</dt>
+                                                        <dd class="col-sm-6">{State.LogModel.current.logId}</dd>
+
+                                                        <dt class="col-sm-6">Subtype:</dt>
+                                                        <dd class="col-sm-6">
+                                                            {State.LogModel.current.subtype === 'run' ?
+                                                                <span class="badge badge-warning">
+                                                                    {State.LogModel.current.subtype}
+                                                                </span>
+                                                                : State.LogModel.current.subtype}
+                                                        </dd>
+
+                                                        <dt class="col-sm-6">Origin:</dt>
+                                                        <dd class="col-sm-6">
+                                                            {State.LogModel.current.origin === 'human' ?
+                                                                <span class="badge badge-success">
+                                                                    {State.LogModel.current.origin}
+                                                                </span>
+                                                                : State.LogModel.current.origin === 'process' ?
+                                                                    <span class="badge badge-primary">
+                                                                        {State.LogModel.current.origin}
+                                                                    </span>
+                                                                    : State.LogModel.current.origin}
+                                                        </dd>
+
+                                                        <dt class="col-sm-6">Creation time:</dt>
+                                                        <dd class="col-sm-6">
+                                                            {format(
+                                                                State.LogModel.current.creationTime,
+                                                                'HH:mm:ss DD/MM/YYYY'
+                                                            )}
+                                                        </dd>
+                                                        <dt class="col-sm-6">Author:</dt>
+                                                        <dd class="col-sm-6">
+                                                            {State.LogModel.current.user &&
+                                                                State.LogModel.current.user.userId}
+                                                        </dd>
+                                                    </dl>
                                                 </div>
                                             </div>
                                         </div>
+                                        <Tabs
+                                            tabs={LogTabs}
+                                            entity={State.LogModel.current}
+                                        />
                                     </div>
-                                    <div class="card-body">
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <h5 class="card-title">{State.LogModel.current.title}</h5>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <dl class="row">
-                                                    <dt class="col-sm-6">Log id</dt>
-                                                    <dd class="col-sm-6">{State.LogModel.current.logId}</dd>
-
-                                                    <dt class="col-sm-6">Subtype:</dt>
-                                                    <dd class="col-sm-6">
-                                                        {State.LogModel.current.subtype === 'run' ?
-                                                            <span class="badge badge-warning">
-                                                                {State.LogModel.current.subtype}
-                                                            </span>
-                                                            : State.LogModel.current.subtype}
-                                                    </dd>
-
-                                                    <dt class="col-sm-6">Origin:</dt>
-                                                    <dd class="col-sm-6">
-                                                        {State.LogModel.current.origin === 'human' ?
-                                                            <span class="badge badge-success">
-                                                                {State.LogModel.current.origin}
-                                                            </span>
-                                                            : State.LogModel.current.origin === 'process' ?
-                                                            <span class="badge badge-primary">
-                                                                {State.LogModel.current.origin}
-                                                            </span>
-                                                            : State.LogModel.current.origin}
-                                                    </dd>
-
-                                                    <dt class="col-sm-6">Creation time:</dt>
-                                                    <dd class="col-sm-6">
-                                                        {format(
-                                                            State.LogModel.current.creationTime,
-                                                            'HH:mm:ss DD/MM/YYYY'
-                                                        )}
-                                                    </dd>
-                                                    <dt class="col-sm-6">Author:</dt>
-                                                    <dd class="col-sm-6">
-                                                        {State.LogModel.current.user &&
-                                                            State.LogModel.current.user.userId}
-                                                    </dd>
-                                                </dl>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <Tabs
-                                        tabs={LogTabs}
-                                        entity={State.LogModel.current}
-                                    />
-                                </div>
+                                </Card>
                             </div>
                         </div>
                     </HttpErrorAlert>
